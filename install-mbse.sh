cfa4b9d2f082234d3129c67ba94413597a002674
Lars Kellogg-Stedman <lars@redhat.com>
2017-03-20 09:37:49 -0400

    put share directory in volume and expose html docs

--- install-mbse.sh
+++ install-mbse.sh
@@ -8,10 +8,11 @@ mkdir $MBSE_ROOT/template
 chown -R mbse:bbs $MBSE_ROOT/ftp
 chmod 775 $MBSE_ROOT/home/bbs
 
-for dir in home etc var log tmp ftp; do
+for dir in share home etc var log tmp ftp; do
 	mv $MBSE_ROOT/$dir $MBSE_ROOT/template
 	ln -s /srv/mbse/$dir $MBSE_ROOT/$dir
 done
 
 install -m 755 -o mbse -g bbs -d /srv/mbse/www/htdocs
 ln -s /srv/mbse/www /var/www
+ln -s /opt/mbse/html /srv/mbse/www/htdocs/docs
