ebbc40dfc17f82f2baa0efcbe7b1cc74ab5411c3
Lars Kellogg-Stedman <lars@redhat.com>
2017-03-19 21:53:44 -0400

    move home and www directories into /srv/mbse

--- install-mbse.sh
+++ install-mbse.sh
@@ -5,7 +5,13 @@
 make install
 mkdir $MBSE_ROOT/template
 
-for dir in etc var log tmp ftp; do
+chown -R mbse:bbs $MBSE_ROOT/ftp
+chmod 775 $MBSE_ROOT/home/bbs
+
+for dir in home etc var log tmp ftp; do
 	mv $MBSE_ROOT/$dir $MBSE_ROOT/template
 	ln -s /srv/mbse/$dir $MBSE_ROOT/$dir
 done
+
+install -m 755 -o mbse -g bbs -d /srv/mbse/www/htdocs
+ln -s /srv/mbse/www /var/www
